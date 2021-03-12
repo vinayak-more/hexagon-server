295ecfc7537ab112cebf71e3995fe0dbe92cf966
Vinayak <vinayak.more@gmail.com>
2021-03-12 10:00:26 +0530

    test

--- src/main/java/com/hexagon/app/controller/VehicleController.java
+++ src/main/java/com/hexagon/app/controller/VehicleController.java
@@ -5,7 +5,6 @@ import com.hexagon.app.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.Collection;
 
 @CrossOrigin("*")
