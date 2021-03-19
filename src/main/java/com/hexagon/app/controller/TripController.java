df9dc0e4ec7659d50a89514b9c4eecdc9bc3dbe0
Vinayak <vinayak.more@gmail.com>
2021-03-19 17:50:49 +0530

    trip controller changes

--- src/main/java/com/hexagon/app/controller/TripController.java
+++ src/main/java/com/hexagon/app/controller/TripController.java
@@ -1,21 +1,21 @@
 package com.hexagon.app.controller;
 
+import com.hexagon.app.model.Trip;
 import com.hexagon.app.service.IEntityService;
 import com.hexagon.app.service.TripService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/trips")
-public class TripController extends IEntityController {
+public class TripController extends IEntityController<Trip> {
 
     @Autowired
     private TripService service;
 
     @Override
-    public IEntityService getService() {
+    public IEntityService<Trip> getService() {
         return service;
     }
 
