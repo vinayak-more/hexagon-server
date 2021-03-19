2db061726449115712bd1b45d106ea8b4e50977f
Vinayak <vinayak.more@gmail.com>
2021-03-19 12:13:55 +0530

    trip entity created

--- src/main/java/com/hexagon/app/model/Trip.java
+++ src/main/java/com/hexagon/app/model/Trip.java
@@ -1,7 +1,106 @@
 package com.hexagon.app.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import java.util.Date;
 
 @Entity
 public class Trip extends IEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", referencedColumnName = "id")
+    private Vehicle vehicle;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_id", referencedColumnName = "id")
+    private Driver driver;
+
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private Client client;
+
+    private String fromLocation;
+    private String toLocation;
+    private long startKm;
+    private long endKm;
+    private double cost;
+    private Date tripDate;
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getFromLocation() {
+        return fromLocation;
+    }
+
+    public void setFromLocation(String fromLocation) {
+        this.fromLocation = fromLocation;
+    }
+
+    public String getToLocation() {
+        return toLocation;
+    }
+
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
+
+    public long getStartKm() {
+        return startKm;
+    }
+
+    public void setStartKm(long startKm) {
+        this.startKm = startKm;
+    }
+
+    public long getEndKm() {
+        return endKm;
+    }
+
+    public void setEndKm(long endKm) {
+        this.endKm = endKm;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Date getTripDate() {
+        return tripDate;
+    }
+
+    public void setTripDate(Date tripDate) {
+        this.tripDate = tripDate;
+    }
+
 }
