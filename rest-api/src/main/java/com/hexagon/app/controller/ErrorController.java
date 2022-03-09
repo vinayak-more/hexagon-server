cd651535d3c53949ec7d11bd8792c18511af126e
Vinayak More <vinayak.more0914@gmail.com>
2022-03-09 13:30:54 +0530

    changes to run app locally

---
+++ rest-api/src/main/java/com/hexagon/app/controller/ErrorController.java
@@ -0,0 +1,21 @@
+package com.hexagon.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "forward:/";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
